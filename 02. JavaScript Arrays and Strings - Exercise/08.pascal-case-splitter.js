eef8291c309aa0498be56df5c1ba1db7b39b3c70
miroLLL <37121246+HristomirAsenov@users.noreply.github.com>
2024-07-03 22:24:24 +0300

    Update 08.pascal-case-splitter.js

--- 02. JavaScript Arrays and Strings - Exercise/08.pascal-case-splitter.js
+++ 02. JavaScript Arrays and Strings - Exercise/08.pascal-case-splitter.js
@@ -1,12 +1,11 @@
 function pascalCaseSplitter (inputString) {
-    const regex = /[A-Z][a-z]/g;
+    const regex = /[A-Z][a-z]*/g;
     const matches = inputString.matchAll(regex);
     const output = [];
-
     for(const match of matches) {
         output.push(match);
     }
-    console.log(output.join(", "));
+    console.log(output.join(", "))
 }
 
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
