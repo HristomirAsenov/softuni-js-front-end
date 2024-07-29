0d8bb393ddf3706915ff4fbcde4e2aac040e1677
miroLLL <hristomir.asenov@gmail.com>
2024-07-29 20:36:06 +0300

    add last problem's solution

--- 05. JavaScript DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ 05. JavaScript DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+  const toEl = document.getElementById("selectMenuTo");
 
-    //TODO...
+  toEl.innerHTML += `
+        <option value="binary">
+            Binary
+        </option>
+        <option value="hexadecimal">
+            Hexadecimal
+        </option>
+        `;
+
+  document.querySelector("button").addEventListener("click", () => {
+    const inputEl = document.getElementById("input");
+
+    const base = {
+      binary: 2,
+      hexadecimal: 16,
+    };
+
+    document.getElementById("result").value = Number(inputEl.value)
+      .toString(base[toEl.value.toLowerCase()])
+      .toUpperCase();
+  });
 }
